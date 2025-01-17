ed964fe38192e27b35b159bfcb1aad2322bd2137
Aryanftw <ap890383@gmail.com>
2025-01-17 20:28:29 +0530

    Updated home page layout and fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import './main.css'
-import Intropage from './components/Intropage'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import './main.css';
+import Intropage from './components/Intropage';
 import Home from './components/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Define routes using createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/", // This corresponds to "/"
+    element: <Intropage />, // This will load at the root
+  },
+  {
+    path: "/home", // This corresponds to "/home"
+    element: <Home />, // This will load at /home
+  },
+]);
 
 const App = () => {
-  return (
-    <BrowserRouter basename="/portfolio">
-      <Routes>
-        <Route path="/" element={<Intropage />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-      
-    </BrowserRouter>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
